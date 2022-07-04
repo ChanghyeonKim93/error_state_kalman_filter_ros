c3327b83308d73d14172e47a518c128e612ff287
ChanghyeonKim93 <hyun91015@gmail.com>
2022-07-05 03:22:02 +0900

    updated.

--- include/error_state_kalman_filter.h
+++ include/error_state_kalman_filter.h
@@ -6,18 +6,18 @@
 
 using namespace Eigen;
 
-typedef Vector3d Vec3;
-typedef Vector4d Vec4;
-typedef Matrix3d Mat33;
-typedef Matrix4d Mat44;
+typedef Matrix<double,3,1> Vec3;
+typedef Matrix<double,4,1> Vec4;
+typedef Matrix<double,3,3> Mat33;
+typedef Matrix<double,4,4> Mat44;
+typedef Matrix<double,3,4> Mat34;
+typedef Matrix<double,4,3> Mat43;
+
 #define POW2(x) ((x)*(x))
 
 class ESKF{
 private:
-    Mat33 I33;
-    Mat44 I44;
-    Mat33 O33;
-    Mat44 O44;
+    // Related to Kalman filter implementation .
 
 public:
     ESKF();
@@ -27,6 +27,12 @@ public:
     void update(); // by optitrack
 
 private:
+    static Mat33 I33;
+    static Mat44 I44;
+    static Mat33 O33;
+    static Mat44 O44;
+    static Mat34 O34;
+    static Mat43 O43;
 
     struct NominalState{
         Vec3 p;
@@ -83,7 +89,7 @@ private:
         Vec3 acc;
         Vec3 gyro;
         Measurement(){
-            acc = Vec3::Zero();
+            acc  = Vec3::Zero();
             gyro = Vec3::Zero();
         };
     };
@@ -102,25 +108,28 @@ private:
         double sig_ng; // gyro measurement noise (0.000006)
         double sig_nba; // acc. bias noise (1e-15)
         double sig_nbg; // gyro bias noise (1e-15)
-        Matrix4d Q;
-        ProcessNoise() : sig_na(0.0008), sig_ng(0.000006), sig_nba(1e-12), sig_nbg(1e-12){
-            Q = Matrix4d::Identity();
-            Q(0,0) = POW2(sig_na);
-            Q(1,1) = POW2(sig_ng);
-            Q(2,2) = POW2(sig_nba);
-            Q(3,3) = POW2(sig_nbg);
-
+        int dim;
+        Matrix<double, 12,12> Q;
+        ProcessNoise() : sig_na(0.0008), sig_ng(0.000006), sig_nba(1e-12), sig_nbg(1e-12), dim(12) {
+            Q = Matrix<double, 12,12>::Identity();
+            for(int i = 0; i < 3; ++i){
+                Q(i,i) = POW2(sig_na);
+                Q(3+i,3+i) = POW2(sig_na);
+                Q(6+i,6+i) = POW2(sig_na);
+                Q(9+i,9+i) = POW2(sig_na);
+            }
          };
     };
 
     struct MeasurementNoise{
         double sig_p; // optitrack position noise // 0.005 (5 mm)
         double sig_q; // quaternion noise // 0.015 ( 0.015 rad)
-        Matrix2d R; 
-        MeasurementNoise() : sig_p(0.005), sig_q(0.015) { 
-            R = Matrix2d::Identity();
-            R(0,0) = POW2(sig_p);
-            R(1,1) = POW2(sig_q);
+        int dim;
+        Matrix<double, 7, 7> R; 
+        MeasurementNoise() : sig_p(0.005), sig_q(0.015), dim(7) { 
+            R = Matrix<double, 7,7>::Identity();
+            for(int i = 0; i < 3; ++i) R(i,i) = POW2(sig_p);
+            for(int i = 0; i < 4; ++i) R(3+i,3+i) = POW2(sig_q);
         };
     };
 
--- src/error_state_kalman_filter.cpp
+++ src/error_state_kalman_filter.cpp
@@ -1,5 +1,13 @@
 #include "error_state_kalman_filter.h"
 
+// Initialize Static member variables
+Mat33 ESKF::I33 = Mat33::Identity();
+Mat44 ESKF::I44 = Mat44::Identity();
+Mat33 ESKF::O33 = Mat33::Zero();
+Mat44 ESKF::O44 = Mat44::Zero();
+Mat34 ESKF::O34 = Mat34::Zero();
+Mat43 ESKF::O43 = Mat43::Zero();    
+
 ESKF::ESKF()
 : measurement_noise_(), process_noise_(), X_nom_(), dX_()
 {
